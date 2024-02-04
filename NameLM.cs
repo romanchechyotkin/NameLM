36f1d594b72096f4ba66245643e46530551030a5
romanchechyotkin <romanchechyotkin@gmail.com>
2024-02-04 18:12:26 +0300

    bigrams

--- NameLM.cs
+++ NameLM.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 
 class NameLM
 {
@@ -7,6 +8,8 @@ class NameLM
         string dataSet = "./names.txt";
 
         List<string> data = ReadDataSet(dataSet);
+        
+        Dictionary<Tuple<string, string>, int> bigrams = GetBigrams(data);
     }
 
     static List<string> ReadDataSet(string path)
@@ -35,6 +38,35 @@ class NameLM
         Debug.WriteLine($"data set length {names.Count()}");
 
         return names;
+    }
+
+    static Dictionary<Tuple<string, string>, int> GetBigrams(List<string> data)
+    {
+        Dictionary<Tuple<string, string>, int> bigrams = new Dictionary<Tuple<string, string>, int>();
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            data[i] = data[i].Trim();
+
+            string[] chars = { "." };
+            string[] words_seq = data[i].Select(c => c.ToString()).ToArray();
+            chars = chars.Concat(words_seq).ToArray();
+            chars = chars.Concat(new string[] { "." }).ToArray();
+
+            var zipped = chars.Zip(chars.Skip(1), (ch1, ch2) => new { Ch1 = ch1, Ch2 = ch2 });
+
+            foreach (var bigram in zipped)
+            {
+                bigrams[Tuple.Create(bigram.Ch1, bigram.Ch2)] = bigrams.GetValueOrDefault(Tuple.Create(bigram.Ch1, bigram.Ch2), 0) + 1;
+            }
+        }
+
+        Debug.WriteLine($"bigrams all amount {bigrams.Count}");
+
+        var maxBigram = bigrams.Aggregate((x, y) => x.Value > y.Value ? x : y);
+        Debug.WriteLine($"max amout of bigram {maxBigram.Key} {maxBigram.Value}");
+
+        return bigrams;
     }
 
 }
